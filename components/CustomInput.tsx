b69eda19e6ed47df0ecdf88c7a37d2475d6d391b
flavius <flaviuscojocaru19@gmail.com>
2024-10-27 21:08:22 +0200

    auth form implementing

---
+++ components/CustomInput.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { authFormSchema } from "@/lib/utils";
+import { z } from "zod";
+
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+}) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                {...field}
+                type={name === "password" ? "password" : "text"}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2"></FormMessage>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
