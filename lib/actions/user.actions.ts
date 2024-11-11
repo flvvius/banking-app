5d5328a0dd4f2ccf6eaf0b8bea29d87895b453e5
flavius <flaviuscojocaru19@gmail.com>
2024-11-11 14:38:26 +0200

    completed the frontend part of sign in / sign up

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export const signIn = async (data: signInProps) => {
+  try {
+  } catch (error) {
+    console.error("Error signing in:", error);
+  }
+};
+
+export const signUp = async (data: SignUpParams) => {
+  try {
+  } catch (error) {
+    console.error("Error signing up:", error);
+  }
+};
